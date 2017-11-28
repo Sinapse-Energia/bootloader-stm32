58de2499726fe69611f053cab5df08c7d1365609
francisjjp <francis@sinapseenergia.com>
2017-11-28 13:32:37 +0100

    FJP:#13.10)Removing M4 dependencies from "Definitions.h"

--- Inc/Definitions.h
+++ Inc/Definitions.h
@@ -13,37 +13,14 @@
 #define CMC_APPLICATION_DEPENDENT
 #define LOG_WIFI  1
 #define NUMBER_RETRIES 2
- /// Dependent Application
-#define START_EEPROM_OWN_CMC_PARAMETERS 	0x10
- /// direcciones dispositivos I2C
-#define I2C__ADDRESS__READING__DS1338    0xD1
-#define I2C__ADDRESS__WRITING__DS1338    0xD0
-#define I2C__ADDRESS__READING__AT24C512  0xA9
-#define I2C__ADDRESS__WRITING__AT24C512  0xA8
 
-#define CIRC_RELEGENERAL_Pin GPIO_PIN_6
-#define CIRC_RELEGENERAL_GPIO_Port GPIOE
-#define CIRC_RELE1_Pin GPIO_PIN_5
-#define CIRC_RELE1_GPIO_Port GPIOE
-#define CIRC_RELE2_Pin GPIO_PIN_4
-#define CIRC_RELE2_GPIO_Port GPIOE
-
-#define CIRC_RELE3_Pin GPIO_PIN_3
-#define CIRC_RELE3_GPIO_Port GPIOE
-#define CIRC_RELE4_Pin GPIO_PIN_2
-#define CIRC_RELE4_GPIO_Port GPIOE
-#define CIRC_RELE5_Pin GPIO_PIN_1
-#define CIRC_RELE5_GPIO_Port GPIOE
- // *************************************************************************************** //
- // *************************************************************************************** //
- // *************************************************************************************** //
 
  // GPRS init
 #define SIZE_APN 						60
 #define SIZE_MAIN_SERVER 				60
 #define SIZE_NTP_SERVER 				60
 #define SIZE_GPRS_BUFFER 				256
-#define SIZE_WIFI_BUFFER 				256
+
 #define TIMING_TIMEOUT_GPRS 			20
 //#define const_APN 						"\"im2m.matooma.com\",\"movistar\",\"movistar\"\r\0"
 #define const_APN 						"\"m2m.tele2.com\",\"tele2\",\"tele2\"\r\0"
@@ -96,17 +73,7 @@
 
 #else
 
- // Define Bank Sectors (16 sectors total 4kb/each - for STM32F0xx devices)
- // Bootloader
- #define FLASH_BANKB_START_SECTOR		FLASH_SECTOR_0
- #define FLASH_BANKB_SECTORS			2
- // Application
- #define FLASH_BANKA_START_SECTOR		FLASH_SECTOR_2
- #define FLASH_BANKA_SECTORS			6
- // Application copy
- #define FLASH_BANKC_START_SECTOR		FLASH_SECTOR_8
- #define FLASH_BANKC_SECTORS			4
-#endif
+
 
 
 // App version addressing (+ release data address dd/mm/yy)
