47efd22ade69dbd07e2d9b3a3907f65384e924ff
juanra <juanramon.bascones@gmail.com>
2018-06-29 09:42:03 +0200

    JRB EXP to slim

--- Inc/Definitions.h
+++ Inc/Definitions.h
@@ -30,8 +30,8 @@
 #define TIMING_TIMEOUT_GPRS 			20
 //#define const_APN 						"\"im2m.matooma.com\",\"movistar\",\"movistar\"\r\0"
 //#define const_APN 						"\"m2m.tele2.com\",\"tele2\",\"tele2\"\r\0"
-//#define const_APN 						"\"matooma.m2m\",\"\",\"\"\0"
-#define const_APN 						"\"lte.m2m\",\"\",\"\"\0"
+#define const_APN 						"\"matooma.m2m\",\"\",\"\"\0"
+//#define const_APN 						"\"lte.m2m\",\"\",\"\"\0"
 //#define const_APN 						"\"orangeworld\",\"orange\",\"orange\"\r\0"
 #define const_SERVER_NTP 				"\"0.europe.pool.ntp.org\"\r\0"
 //#define const_MAIN_SERVER				"\"m2m.eclipse.org\",1883\r\0"
@@ -59,7 +59,11 @@
 #define const_ID_DEVICE					"600012\0"
 #define const_GPIO					"10000000\0"
 #define const_PWM					"100\0"
-#define HTTP_SERVER_FW_FILENAME		"600012.bin\0"
+//#define HTTP_SERVER_FW_FILENAME		"600012.bin\0"
+#define HTTP_SERVER_FW_FILENAME		"TESTING_M2M.bin\0"
+//#define HTTP_SERVER_FW_FILENAME		"EC-M2M-LU_LUM-V114-NODEBUG.bin\0"
+//#define HTTP_SERVER_FW_FILENAME		"EC-M2M-LU_LUM-V114-DEBUG.bin\0"
+
 #define const_ROUTE_FW_FILENAME     ""
 #define const_UPDFW_COUNT			"1\0"
 #define const_UPDFW					"1\0"
